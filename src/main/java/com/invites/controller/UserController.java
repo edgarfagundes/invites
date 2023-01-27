a480d2e2ff62f5bee82740a7b56e028b6a276098
edgarfagundes <edgarfagundessss@gmail.com>
2023-01-27 17:41:31 -0300

    Commit com teclado novo :)

--- src/main/java/com/invites/controller/UserController.java
+++ src/main/java/com/invites/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.invites.controller;
 
-import com.electronwill.nightconfig.core.conversion.Path;
 import com.invites.model.entity.User;
 import com.invites.model.records.UserRecord;
 import com.invites.model.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +23,18 @@ public class UserController {
     }
 
     @PostMapping("/atualizar/{id}")
-    public ResponseEntity<?> atualizaUsuario(@RequestBody @Valid UserRecord userRecord, @PathVariable Long id){
-        return userService.update(new User(userRecord), id);
+    public void atualizaUsuario(@RequestBody @Valid UserRecord userRecord, @PathVariable Long id){
+         userService.update(new User(userRecord), id);
     }
 
     @PostMapping("/delete/{id}")
-    public void deleteUsuario(@PathVariable Long id){}
+    public void deleteUsuario(@PathVariable Long id){
+        userService.delete(id);
+    }
+
+    @GetMapping
+    public Page<User> findUsuarios(Pageable pageable){
+        return userService.findAll(pageable);
+    }
 
 }
--- src/main/java/com/invites/model/services/UserService.java
+++ src/main/java/com/invites/model/services/UserService.java
@@ -3,6 +3,8 @@ package com.invites.model.services;
 import com.invites.model.entity.User;
 import com.invites.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,26 +17,34 @@ public class UserService {
 
     public ResponseEntity<?> save(User user){
         try {
-            return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+            if(user.getId() != null) {
+                return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+            }else {
+                throw new Exception();
+            }
         }catch (Exception e){
              return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    public ResponseEntity<?> update(User user, Long id){
+    public void update(User user, Long id){
         try {
             if (user.getId().equals(id)) {
-                return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+                 new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
             }else {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
         }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     public void delete(Long id){
         userRepository.deleteById(id);
         new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 }
